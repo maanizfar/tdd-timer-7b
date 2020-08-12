951ad8630eee8ab6729a649461229951b0967cfb
Salman Zafar <maanizfar@gmail.com>
2020-08-12 20:27:24 +0500

    update tests

--- src/components/TimerButton.spec.tsx
+++ src/components/TimerButton.spec.tsx
@@ -4,15 +4,24 @@ import TimerButton from "./TimerButton";
 
 describe("TimerButton", () => {
   let container: ShallowWrapper;
+  const clickFn = jest.fn();
 
   beforeEach(
     () =>
       (container = shallow(
-        <TimerButton buttonAction={jest.fn()} buttonValue={""} />
+        <TimerButton buttonAction={clickFn} buttonValue={""} />
       ))
   );
 
   it("should render a <div/>", () => {
     expect(container.find("div").length).toBeGreaterThanOrEqual(1);
+  });
+
+  it("button should call function", () => {
+    expect(clickFn).toHaveBeenCalledTimes(0);
+
+    container.find("div").simulate("click");
+
+    expect(clickFn).toHaveBeenCalledTimes(1);
   });
 });
